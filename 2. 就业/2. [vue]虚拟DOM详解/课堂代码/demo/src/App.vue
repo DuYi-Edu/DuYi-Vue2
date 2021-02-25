e79afb37463f608492c126c07390043a3fc95c22
yjisme <277739025@qq.com>
2021-02-25 15:47:40 +0800

    2. [vue]虚拟DOM详解

---
+++ 2. 就业/2. [vue]虚拟DOM详解/课堂代码/demo/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app" a="1" b="2">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  mounted() {
+    console.log("App", this._vnode);
+  },
+  // render(h){
+  //   return h("div", [h("h1", "title1"), h("h2", "title2")])
+  // }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 2. 就业/2. [vue]虚拟DOM详解/课堂代码/demo/src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello" :a="msg" @click="msg = 'abc'">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  render(h) {
+    return h(
+      "div",
+      {
+        class: "hello",
+        attrs: {
+          a: this.msg,
+        },
+        on: {
+          click: () => {
+            this.msg = "abc";
+          },
+        },
+      },
+      this.msg
+    );
+  },
+  mounted() {
+    console.log("HelloWorld", this._vnode);
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
